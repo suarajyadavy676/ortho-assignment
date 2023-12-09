68a0b302503b2a93ad2ebd1f192b3710c68dfd14
Suraj Yadav <surajyadavy676@gmail.com>
2023-12-09 21:29:51 +0530

    some changes in password

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ let accountCreate = document.getElementById("create")
 let username = document.querySelector(".email")
 let key = document.querySelector(".password")
 
-key.addEventListener("input",function(){
-  let num = key.value
-  if(num == localStorage.getItem("password")){
-    document.querySelector(".login").addEventListener("click",function(){
-      window.location.href ="blog.html"
-    })
-  }
-})
+// key.addEventListener("input",function(){
+//   let num = key.value
+//   if(num == localStorage.getItem("password")){
+//     document.querySelector(".login").addEventListener("click",function(){
+//       window.location.href ="blog.html"
+//     })
+//   }
+// })
 
 document.querySelector(".login").addEventListener("click",function(){
-  // key.addEventListener("input",function(){
-  //   console.log(key.value)
-  //   if(key.value == localStorage.getItem("password")){
-  //     window.location.href ="https://onecompiler.com/html/3zvw8ftgr"
-  //   } else{
-  //     alert("inter currect")
-  //   }
-  // })
+  if(username.value == localStorage.getItem("email") && key.value == localStorage.getItem("password")){
+    window.location.href ="blog.html"
+  } else{
+    alert("please enter currect details")
+  }
 })
 accountCreate.addEventListener('click',function(){
   window.location.href = "signup.html"
